bbea619392b9f82a234b0b485607eb6062d793c9
lukehasawii <may21luke@yahoo.com>
2020-03-21 20:51:08 -0500

    e

--- app/src/main/java/com/projectreborn/tweetox/MainActivity.java
+++ app/src/main/java/com/projectreborn/tweetox/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
+                .build(); //e
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
